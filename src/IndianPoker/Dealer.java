efad236fd0075139985802bbc6daef12953c2d97
sechun <sechun0215@gmail.com>
2018-04-16 13:50:59 +0900

    version 1.1

--- src/IndianPoker/Dealer.java
+++ src/IndianPoker/Dealer.java
@@ -1,11 +1,11 @@
 package IndianPoker;
 
 public class Dealer {
-	private int chip1;
-	private int chip2;
-	private int allChip;
-	private int gamer1Bet;
-	private int gamer2Bet;
+	private int chip1; // gamer1이 betting한 모든 chip
+	private int chip2; // gamer2의 betting한 모든 chip
+	private int allChip; // 두 gamer가 betting한 모든 chip
+	private int gamer1Bet; // gamer1가 betting한 chip
+	private int gamer2Bet; // gamer2가 betting한 chip
 
 	public Dealer(){
 		this.chip1 = 0;
@@ -18,29 +18,42 @@ public class Dealer {
 		return this.allChip;
 	}
 
+	// 처음 betting 받기 전 기본 chip을 받는 메소드
 	public int getChip(Gamer gamer){
 		return gamer.minusChip(1);
 	}
 
+	// game turn을 진행하는 메소드
 	public void runGame(Gamer gamer1, Gamer gamer2) throws Exception{
-		int gamer1Card = gamer1.getCard();
-		int gamer2Card = gamer2.getCard();
+		int gamer1Card = gamer1.getCard(); // gamer1의 카드를 받는 메소드
+		int gamer2Card = gamer2.getCard(); // gamer2의 카드를 받는 메소드
 
+		// gamer1과 gamer2가 AutoGamer 객체일 경우 자신과 상대방의 카드를 기억할 수 있는 메소드를 실행
 		if(gamer2 instanceof AutoGamer){
 			AutoGamer newGamer2 = (AutoGamer)gamer2;
-			newGamer2.addMyCard(gamer2Card); //
-			newGamer2.addOpponentCard(gamer1Card); //	
+			newGamer2.addMyCard(gamer2Card); // 본인 카드
+			newGamer2.addOpponentCard(gamer1Card); // 상대 카드
 		}
 
-		this.gamer1Bet = -1; // 판이 시작할 때 마다
-		this.gamer2Bet = -1; // 0일 경우 check에서 die로 인식하기 때문
+		if(gamer1 instanceof AutoGamer){
+			AutoGamer newGamer1 = (AutoGamer)gamer1;
+			newGamer1.addMyCard(gamer1Card); // 본인 카드
+			newGamer1.addOpponentCard(gamer2Card); // 상대 카드
+		}
 
+		// 두 gamer의 초기 betting 값을 -1로 설정, 0일 경우 check 메소드에서 die로 인식
+		this.gamer1Bet = -1;
+		this.gamer2Bet = -1;
+
+		// 두 gamer가 chip을 1씩 betting을 하게 함
 		this.chip1 = this.getChip(gamer1);
 		this.chip2 = this.getChip(gamer2);
 
+		// turn 진행
 		this.turn(gamer1, gamer2, gamer1Card, gamer2Card);
 	}
 
+	// 현재 두 gamer가 betting한 칩을 보여주는 메소드
 	public void printNowChips(Gamer gamer1, Gamer gamer2){
 		System.out.println("\n******현재 배팅한 칩******");
 		System.out.println(gamer1.getName() + " : " + this.chip1);
@@ -48,6 +61,7 @@ public class Dealer {
 		System.out.println("**********************");
 	}
 	
+	// 판이 끝난후 두 gamer의 카드를 보여주는 메소드
 	public void printCard(Gamer gamer1, Gamer gamer2, int gamer1Card, int gamer2Card){
 		System.out.println("\n########################");
 		System.out.println(gamer1.getName()+" 의 카드 : " + gamer1Card);
@@ -55,51 +69,57 @@ public class Dealer {
 		System.out.println("########################\n");
 	}
 
+	// 각 gamer의 betting turn을 진행하는 메소드
 	public void turn(Gamer gamer1, Gamer gamer2, int gamer1Card, int gamer2Card){
 		while(true){
 			this.printNowChips(gamer1, gamer2); // 현재 배팅된 칩 상태
-			this.gamer2Bet = gamer2.betting(gamer1Card, this.chip1, this.chip2); // 컴퓨터 배팅
+			this.gamer2Bet = gamer2.betting(gamer1Card, this.chip1, this.chip2); // gamer2 배팅
 			this.chip2 += this.gamer2Bet; // 배팅한 칩을 현재 배팅된 칩 갯수에 더 해줌 
 
 			if(this.check(gamer1, gamer2, gamer1Card, gamer2Card)) break; // 판을 끝낼 수 있는지 조건 확인
 
 			this.printNowChips(gamer1, gamer2); // 현재 배팅된 칩 상태
-			this.gamer1Bet = gamer1.betting(gamer2Card, this.chip2, this.chip1); // 게이머 배팅		
+			this.gamer1Bet = gamer1.betting(gamer2Card, this.chip2, this.chip1); // gamer1 배팅		
 			this.chip1 += this.gamer1Bet; // 배팅한 칩을 현재 배팅된 칩 갯수에 더해줌
 
 			if(this.check(gamer1, gamer2, gamer1Card, gamer2Card)) break; // 판을 끝낼 수 있는지 조건 확인
 		}
 	}
 
+	// betting이 끝난 후 판이 끝날 수 있는 조건을 확인하는 메소드
 	public boolean check(Gamer gamer1, Gamer gamer2, int gamer1Card, int gamer2Card){
-		if(this.gamer1Bet == 0 || this.gamer2Bet == 0){ // 한 쪽 게이머가 die하였을 때 
-			if(this.gamer1Bet==0){
+		// 한 쪽 gamer가 die하였을 때 
+		if(this.gamer1Bet == 0 || this.gamer2Bet == 0){ 
+			if(this.gamer1Bet==0){ // gamer1이 die 하였을 때
 				System.out.println(gamer1.getName() + " : Die 하였습니다.");
-				gamer2.pluChips(this.getAllChips());
+				gamer2.plusChips(this.getAllChips()); // gamer2가 모든 chip을 가져감
 				this.printCard(gamer1, gamer2, gamer1Card, gamer2Card);
-			}else{
+			}else{ // gamer2가 die 하였을 때
 				System.out.println(gamer2.getName() + " : Die 하였습니다.");
-				gamer1.pluChips(this.getAllChips());
+				gamer1.plusChips(this.getAllChips()); // 
 				this.printCard(gamer1, gamer2, gamer1Card, gamer2Card);
 			}
-			return true;
-		}else if (this.chip1 == this.chip2) { // 둘 다 콜 하였을 때 
+			return true; // 판 종료
+		// 두 gamer 모두 call 하였을 때 
+		}else if (this.chip1 == this.chip2) { 
 			if(gamer1Card == gamer2Card){ // 비겼을 경우
-				gamer1.pluChips(this.getAllChips()/2);
-				gamer2.pluChips(this.getAllChips()/2);
+				// 두 gamer에게 동일하게 chip을 배분함
+				gamer1.plusChips(this.getAllChips()/2);
+				gamer2.plusChips(this.getAllChips()/2);
 				this.printCard(gamer1, gamer2, gamer1Card, gamer2Card);
 			}else{ // 한 쪽이 이긴 경우
 				if(gamer1Card < gamer2Card){
-					gamer2.pluChips(this.getAllChips());
+					gamer2.plusChips(this.getAllChips());
 					this.printCard(gamer1, gamer2, gamer1Card, gamer2Card);
 				}else{
-					gamer1.pluChips(this.getAllChips());
+					gamer1.plusChips(this.getAllChips());
 					this.printCard(gamer1, gamer2, gamer1Card, gamer2Card);
 				}
 			}
-			return true;
+			return true; // 판 종료
 		}else{
-			return false;
+			// game 진행 (한 쪽 gamer가 raise를 하여 betting한 chip 수를 올렸기 때문에 위 조건에 충족하지 않음)
+			return false; 
 		}
 	}
 }
--- src/IndianPoker/Gamer.java
+++ src/IndianPoker/Gamer.java
@@ -7,12 +7,13 @@ import java.util.Random;
 
 public class Gamer {
 
-	private ArrayList<Integer> deck;
-	private String name;
+	private ArrayList<Integer> deck; // 1~10 까지 숫자가 2장씩 들어갈 deck
+	private String name; // gamer의 이름
 	private int chip;
-	private int deckSize;
+	private int deckSize; // deck의 크기
 
 	public Gamer() {}
+	
 	public Gamer(String name, int chip){
 		this.deckSize = 20;
 		this.deck = makeDeck();
@@ -24,19 +25,22 @@ public class Gamer {
 		return this.name;
 	}
 
-	public void pluChips(int chip){
+	// chip을 얻어오는 메소드
+	public void plusChips(int chip){
 		this.chip += chip;
 		System.out.println("===================");
-		System.out.println(this.getName() + "이 승리하였습니다. \n승자 보유 칩:" + this.chip);
+		System.out.println(this.getName() + "이 칩을 얻습니다. \n" + this.getName() + "보유 칩:" + this.chip);
 		System.out.println("===================");
 	}
 
+	// deck에서 랜덤으로 카드를 반환하는 메소드
 	public int getCard() throws IllegalArgumentException{
 		Random rand = new Random();
 		int index = rand.nextInt(this.deckSize--);
 		return this.deck.remove(index);
 	}
 
+	// 1~10 까지 2개씩 숫자가 들어있는 ArrayList를 만들어 반환하는 메소드
 	public ArrayList<Integer> makeDeck(){
 		ArrayList<Integer> temp = new ArrayList<Integer>();
 		for(int i=0; i<2; i++){
@@ -47,6 +51,7 @@ public class Gamer {
 		return temp;
 	}
 
+	// deck이 비어있는지 아닌지 확인하는 메소드
 	public boolean isEmpty(){
 		if(this.deckSize==0){
 			return true;
@@ -55,16 +60,19 @@ public class Gamer {
 		}
 	}
 
+	// 현재 gamer의 보유 칩 갯수를 반환하는 메소드
 	public int chipCount(){
 		return this.chip;
 	}
 
+	// gamer의 보유 칩에서 필요한 칩만 큼 빼는 메소드
 	public int minusChip(int chip){
 		this.chip -= chip;
 		System.out.println(this.getName() + "의 보유 칩:" + this.chip);
 		return chip;
 	}
 
+	// gamer가 betting하는 메소드
 	public int betting(int opponentCard, int opponentChip,int myChip){
 		System.out.println("\n-->"+ this.getName()+"의 Turn");
 		System.out.println("\n상대의 카드:"+ opponentCard);
@@ -76,47 +84,53 @@ public class Gamer {
 		int chip = 0;
 
 		switch(menu){
-		case 1:
+		case 1: // call
 			while(true){
 				System.out.println("++배팅할 칩 갯수++");
 				chip = Integer.parseInt(readFromKeyboard());
-				if(opponentChip == 1){
+				if(opponentChip == 1){ // 상대가 아직 betting 안한 경우 
 					if(this.chipCount() > chip){
 						System.out.println("Call하였습니다.");
 						return this.minusChip(chip);
 					}else{
-						System.out.println("@@잘못 입력하였습니다. 다시입력해주세요.");
+						System.out.println("#잘못 입력하였습니다. 다시입력해주세요.");
 					}
 				}else{
 					if(this.chipCount() > chip && (chip + myChip) == opponentChip){
 						System.out.println("Call 하였습니다.");
 						return this.minusChip(chip);
+					}else if(this.chipCount() < chip){
+						System.out.println("#Call할 수 없습니다. Die합니다.");
+						return 0;
 					}else{
-						System.out.println("@@잘못 입력하였습니다. 다시입력해주세요.");
+						System.out.println("#잘못 입력하였습니다. 다시입력해주세요.");
 					}
 				}
 			}
-		case 2:
+		case 2: // raise
 			while(true){
 				System.out.println("++배팅할 칩 갯수++");
 				chip = Integer.parseInt(readFromKeyboard());
-				if(opponentChip == 1){
+				if(opponentChip == 1){ // 상대가 아직 betting 안한 경우 
 					if(this.chipCount() > chip){
 						System.out.println("Raise 하였습니다.");
 						return this.minusChip(chip);
 					}else{
-						System.out.println("@@잘못 입력하였습니다. 다시입력해주세요.");
+						System.out.println("#잘못 입력하였습니다. 다시입력해주세요.");
 					}
 				}else{
 					if(this.chipCount() > chip && (chip + myChip) > opponentChip){
 						System.out.println("Raise하였습니다.");
 						return this.minusChip(chip);
+					}else if(this.chipCount() < chip){
+						System.out.println("#Raise할 수 없습니다. Die합니다.");
+						return 0;
 					}else{
-						System.out.println("@@잘못 입력하였습니다. 다시입력해주세요.");
+						System.out.println("#잘못 입력하였습니다. 다시입력해주세요.");
 					}
 				}
 			}
-		case 3:
+		case 3: // die
 			return 0;
 		default:
 			System.out.println("@@잘못 입력하셨습니다.");
@@ -124,8 +138,7 @@ public class Gamer {
 		}
 
 	}
-
-
+	
 	public String readFromKeyboard(){
 		String input = null;
 		try{
